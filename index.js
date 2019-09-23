73d32e7d21746c9e54b1e9cd3c978c7cd73358af
Jason <jas0nle@yahoo.com>
2019-09-23 15:02:38 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let winningObject = array.find( object => {
+    return object.result === "W";
+  })
+  if (winningObject) {
+    return winningObject.year;
+  }
+}
